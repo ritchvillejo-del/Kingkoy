41110e11c44b2e748634b0f3d2ef95fc01d68a39
ritchvillejo-del <ritch_villejo@gmail.com>
2026-03-26 10:08:09 +0800

    Add 3 console writeline

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Walang aalis!");
+Console.WriteLine("Walang aatras!");
+Console.WriteLine("Walang susuko!");
